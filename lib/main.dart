05ec49a3451e81fc2251559977c770429516e289
ankurrajsonowal <ankurrajsonowal110@gmail.com>
2026-09-23 08:37:58 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,12 @@
 import 'package:flutter/material.dart';
-import 'controller_screen.dart';
-import 'connection_service.dart';
-
-void main() {
-  runApp(const PhonePadApp());
-}
-
-class PhonePadApp extends StatelessWidget {
-  const PhonePadApp({super.key});
+void main() => runApp(const MaterialApp(home: MyApp()));
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: const HomeSelector(),
-    );
-  }
-}
-
-class HomeSelector extends StatelessWidget {
-  const HomeSelector({super.key});
-  @override
-  Widget build(BuildContext context) {
-    final conn = ConnectionService();
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => ControllerScreen(connection: conn))),
-          child: const Text("START CONTROLLER"),
-        ),
-      ),
+      body: Center(child: Text("PhonePad Controller Ready!", style: TextStyle(color: Colors.white, fontSize: 22))),
     );
   }
 }
